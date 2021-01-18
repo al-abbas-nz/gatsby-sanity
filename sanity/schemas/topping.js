d79179c6cb7f0b3365bf2d9e84c2d1a61ca4e14b
al-abbas-nz <al.mus930@gmail.com>
2021-01-18 21:13:33 +1300

    add sannity schemas

---
+++ sanity/schemas/topping.js
@@ -0,0 +1,37 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // Computer name
+  name: 'topping',
+
+  // Visible title
+  title: 'Topping',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'What is the name of the topping?',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is it vege brah?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
